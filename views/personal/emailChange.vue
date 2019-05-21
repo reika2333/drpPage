7c94da6628ed47a759182efbcd083c497259c190
刘岭柯 <reika2333@yahoo.co.jp>
2019-05-21 09:07:47 +0800

    llk 添加密码邮箱修改页 公钥搜索结果

--- views/personal/emailChange.vue
+++ views/personal/emailChange.vue
@@ -1,5 +1,70 @@
 <template>
-    
+    <div>
+        <div class="info-card">
+            <div class="info-card-head">
+                <h3>修改邮箱</h3>
+            </div>
+            <Form class="info-card-body" label-width="80">
+                <Row style="height: 150px;margin-top: 10px">
+                    <Col span="20" style="height: 45px">
+                        <formItem label="新邮箱：">
+                            <Input readonly></Input>
+                        </formItem>
+                    </Col>
+                    <Col span="4" style="height: 45px;text-align: center">
+                        <Button type="default">发送验证码</Button>
+                    </Col>
+                    <Col span="20" style="height: 45px">
+                        <formItem label="验证码：">
+                            <Input readonly>
+                            </Input>
+                        </formItem>
+                    </Col>
+                    <Col span="20" style="text-align: center">
+                        <Button type="primary">提交修改</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+        <div class="info-card">
+            <div class="info-card-head">
+                <h3>修改密码</h3>
+            </div>
+            <Form class="info-card-body" label-width="80">
+                <Row style="height: 220px;margin-top: 10px">
+                    <Col span="20" style="height: 45px">
+                        <formItem label="旧密码：">
+                            <Input  type="password"></Input>
+                        </formItem>
+                    </Col>
+                    <Col span="20" style="height: 45px">
+                        <formItem label="新密码：">
+                            <Input  type="password">
+                            </Input>
+                        </formItem>
+                    </Col>
+                    <Col span="20" style="height: 45px">
+                        <formItem label="确认密码：">
+                            <Input  type="password">
+                            </Input>
+                        </formItem>
+                    </Col>
+                    <Col span="20" style="height: 45px">
+                        <formItem label="验证码：">
+                            <Input>
+                            </Input>
+                        </formItem>
+                    </Col>
+                    <Col span="4" style="height: 45px;text-align: center">
+                        <Button type="default">发送验证码</Button>
+                    </Col>
+                    <Col span="20" style="text-align: center">
+                        <Button type="primary">提交修改</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+    </div>
 </template>
 
 <script>
---
+++ views/personal/publicKey.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="info-card">
+        <div class="info-card-head">
+            <h3>公钥查找结果</h3>
+        </div>
+        <div class="info-card-body">
+            <!--列表-->
+            <Table class="mytable" :columns="myColumns" :data="myAgentList"></Table>
+            <!--分页-->
+            <Row style="height: 40px;margin-top: 10px">
+                <Col style="text-align: right">
+                    <Page :total="10"
+                          show-sizer/>
+                </Col>
+            </Row>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "publicKey",
+        data: function () {
+            return {
+                myColumns: [
+                    {
+                        title: '序号',
+                        type: 'index',
+                        align: 'center',
+                        width: 80
+                    },
+                    {
+                        title: '用户名',
+                        key: 'works_name',
+                        align: 'center'
+                    },
+                    {
+                        title: '用户ID',
+                        key: 'works_name',
+                        align: 'center'
+                    },
+                    {
+                        title: '公钥',
+                        key: 'works_name',
+                        align: 'center'
+                    },
+                    {
+                        title: '操作',
+                        key: 'works_name',
+                        align: 'center',
+                        render: (h, params) => {
+                            return  h('div', [
+                                h('div', {
+                                    domProps: {
+                                        innerHTML: `<a src="">下载</a>`
+                                    }
+                                })
+                            ])
+                        }
+                    }
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
